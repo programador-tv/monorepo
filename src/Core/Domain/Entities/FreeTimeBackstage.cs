bd8e94efc21f2bfb4a8123170210d79135295a71
serros404 <serros404@gmail.com>
2024-07-28 19:23:21 -0300

    Fix: Apagar arquivo PerfilService.cs (https://github.com/orgs/programador-tv/projects/5/views/1?pane=issue&itemId=72415605)

--- src/Core/Domain/Entities/FreeTimeBackstage.cs
+++ src/Core/Domain/Entities/FreeTimeBackstage.cs
@@ -2,14 +2,22 @@
 
 namespace Domain.Entities;
 
-public sealed class FreeTimeBackstage(Guid id, Guid timeSelectionId, int maxParticipants, bool ilimitado)
-    : Entity(id)
+public sealed class FreeTimeBackstage(
+    Guid id,
+    Guid timeSelectionId,
+    int maxParticipants,
+    bool ilimitado
+) : Entity(id)
 {
     public Guid TimeSelectionId { get; private set; } = timeSelectionId;
     public int MaxParticipants { get; private set; } = maxParticipants;
     public bool Ilimitado { get; private set; } = ilimitado;
 
-    public static FreeTimeBackstage Create(Guid timeSelectionId, int maxParticipants, bool ilimitado)
+    public static FreeTimeBackstage Create(
+        Guid timeSelectionId,
+        int maxParticipants,
+        bool ilimitado
+    )
     {
         return new FreeTimeBackstage(Guid.NewGuid(), timeSelectionId, maxParticipants, ilimitado);
     }
