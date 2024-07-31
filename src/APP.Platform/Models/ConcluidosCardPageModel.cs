87ab3f7099fa2d64ad11e7cf3e76022043d1d18a
luizmarcelolm <luizmarcellolm@gmail.com>
2024-07-31 11:19:26 -0300

    alt avatar redirect for canal

--- src/APP.Platform/Models/ConcluidosCardPageModel.cs
+++ src/APP.Platform/Models/ConcluidosCardPageModel.cs
@@ -5,6 +5,5 @@ public sealed class ConcluidosCardPageModel : BaseCardPageModel
     public string FotoEnvolvido { get; set; } = string.Empty;
     public string NomeEnvolvido { get; set; } = string.Empty;
     public string TempoRestante { get; set; } = string.Empty;
-    public string UserName { get; set; } = string.Empty; //Inserido UserName
-
+    public string UserName { get; set; } = string.Empty;
 }
