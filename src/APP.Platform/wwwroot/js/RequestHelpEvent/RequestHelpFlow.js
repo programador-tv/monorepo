397d92934e06d8041e3ffcf154fce6ffad0e71b4
Pedro Antonio <pedromila2000@gmail.com>
2024-08-06 11:03:13 -0300

    Layout - missing validations and small bugs

---
+++ src/APP.Platform/wwwroot/js/RequestHelpEvent/RequestHelpFlow.js
@@ -0,0 +1,216 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let dateFormRH = document.getElementById("dateTimeRH");
+
+  let submitFormRh = document.getElementById("saveTimeFormHelp");
+
+  let hoursStartRH = document.getElementById("hoursStartRH");
+  let minutesStartRH = document.getElementById("minutesStartRH");
+  let isAmStartRH = document.getElementById("ampmStartRH");
+
+  let inicioHorarioRH = `${
+    hoursStartRH.value < 10 ? "0" + hoursStartRH.value : hoursStartRH.value
+  }:${
+    minutesStartRH.value < 10
+      ? "0" + minutesStartRH.value
+      : minutesStartRH.value
+  }`;
+  console.log(inicioHorarioRH);
+
+  let hoursEndRH = document.getElementById("hoursEndRH");
+  let minutesEndRH = document.getElementById("minutesEndRH");
+  let isAmEndRH = document.getElementById("ampmEndRH");
+
+  let fimHorarioRH = `${
+    hoursEndRH.value < 10 ? "0" + hoursEndRH.value : hoursEndRH.value
+  }:${minutesEndRH.value < 10 ? "0" + minutesEndRH.value : minutesEndRH.value}`;
+  console.log(inicioHorarioRH);
+
+  let tituloResumoRH = document.getElementById("tituloResumoRH");
+  let horariosResumoRH = document.getElementById("horarios-resumoRH");
+  let diaResumoRH = document.getElementById("dia-resumoRH");
+
+  let modalTagsRH = document.getElementById("modal-tagsRH");
+
+  const saveTimeFormRH = document.getElementById("saveTimeFormHelp");
+
+  function SaveTime(aspForm, _hideEventModal) {
+    $(".eventModal").modal("hide");
+    const formData = new FormData(aspForm);
+
+    formData.set(
+      "ScheduleTimeSelection.StartTime",
+      `${dateFormRH.value}T${inicioHorarioRH}-03:00`
+    );
+    formData.set(
+      "ScheduleTimeSelection.EndTime",
+      `${dateFormRH.value}T${fimHorarioRH}-03:00`
+    );
+
+    const url = "/ScheduleActions?handler=SaveTime";
+    const options = {
+      method: "POST",
+      body: formData,
+    };
+
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Erro: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const content = data.content;
+        createTimeModal(content.id);
+        calendar.addEvent(content);
+        firstForm = content;
+        alertTimeSelectionCreatedSucessfully(content.id);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  submitFormRh.addEventListener("submit", (event) => {
+    console.log("chamado");
+    event.preventDefault();
+    const selectElementRH = document.getElementById("TagsSelectedHelp");
+    const selectedOptionsRH = selectElement.selectedOptions;
+    const hideEventModalRH = selectedOptionsRH.length !== 0;
+
+    //limpar os valores
+
+    SaveTime(saveTimeFormRH, hideEventModalRH);
+  });
+
+  const switchStep = (currentStep, nextStep) => {
+    $(currentStep).removeClass("active");
+    $(nextStep).addClass("active");
+  };
+
+  // Passa da etapa 1 para a etapa 2
+  $("#btnRequestHelp1").click(() => {
+    console.log("click botao 1");
+    switchStep(".body-requestHelp-1", ".body-requestHelp-2");
+  });
+
+  // Passa da etapa 2 para a etapa 3
+  $("#btnRequestHelp2").click(() => {
+    switchStep(".body-requestHelp-2", ".body-requestHelp-3");
+  });
+
+  // Passa da etapa 2 para a etapa 3
+  $("#btnRequestHelp3").click(() => {
+    switchStep(".body-requestHelp-3", ".body-requestHelp-4");
+
+    let tituloRH = document.getElementById(
+      "TimeSelectionMentoriaTituloRH"
+    ).value;
+    tituloResumoRH.innerText = tituloRH;
+    horariosResumoRH.innerText = inicioHorarioRH + " - " + fimHorarioRH;
+
+    const date = new Date(dateFormRH.value);
+    date.setDate(date.getDate() + 1);
+
+    const day = date.getDate();
+    const dayOfWeek = date.getDay();
+    const daysOfWeek = [
+      "Domingo",
+      "Segunda-feira",
+      "Terça-feira",
+      "Quarta-feira",
+      "Quinta-feira",
+      "Sexta-feira",
+      "Sábado",
+    ];
+    const dayName = daysOfWeek[dayOfWeek];
+    diaResumoRH.innerText = day.toString() + " " + dayName;
+
+    const ulElement = document.getElementById("select2-TagsSelected-container");
+    const liElements = ulElement.querySelectorAll("li");
+
+    const mainTag = document.createElement("span");
+    mainTag.className = "modal-tag-main";
+    mainTag.textContent = "Ajuda";
+    modalTagsRH.appendChild(mainTag);
+
+    liElements.forEach((li) => {
+      const span = document.createElement("span");
+      span.className = "modal-tag";
+      span.textContent = li.textContent.substring(1).trim();
+      modalTagsRH.appendChild(span);
+    });
+  });
+
+  hoursStartRH.addEventListener("change", updateTimeStartRH);
+  minutesStartRH.addEventListener("change", updateTimeStartRH);
+  isAmStartRH.addEventListener("change", updateTimeStartRH);
+  hoursEndRH.addEventListener("change", updateTimeEndRH);
+  minutesEndRH.addEventListener("change", updateTimeEndRH);
+  isAmEndRH.addEventListener("change", updateTimeEndRH);
+
+  function updateTimeStartRH() {
+    let hoursStartInt = parseInt(hoursStartRH.value, 10);
+    console.log(isAmStartRH.value);
+    if (isAmStartRH.value === "pm" && hoursStartInt !== 12) {
+      hoursStartInt += 12;
+    } else if (isAmStartRH.value === "AM" && hoursStartInt === 12) {
+      hoursStartInt = 0; // Ajuste para 12 AM ser 00
+    }
+
+    let minutesInt = parseInt(minutesStartRH.value, 10);
+
+    inicioHorarioRH = `${
+      hoursStartInt < 10 ? "0" + hoursStartInt : hoursStartInt
+    }:${minutesInt < 10 ? "0" + minutesInt : minutesInt}`;
+
+    console.log(inicioHorarioRH);
+  }
+
+  function updateTimeEndRH() {
+    let hoursEndInt = parseInt(hoursEndRH.value, 10);
+    console.log(isAmEndRH.value);
+    if (isAmEndRH.value === "pm" && hoursEndInt !== 12) {
+      hoursEndInt += 12;
+    } else if (isAmEndRH.value === "AM" && hoursEndInt === 12) {
+      hoursEndInt = 0; // Ajuste para 12 AM ser 00
+    }
+
+    let minutesEndInt = parseInt(minutesEndRH.value, 10);
+
+    fimHorarioRH = `${hoursEndInt < 10 ? "0" + hoursEndInt : hoursEndInt}:${
+      minutesEndInt < 10 ? "0" + minutesEndInt : minutesEndInt
+    }`;
+
+    console.log(fimHorarioRH);
+  }
+
+  // Passa da etapa 3 para a etapa 2
+  $("#btnRequestHBack1").click(() => {
+    console.log("click botao 1");
+
+    switchStep(".body-requestHelp-2", ".body-requestHelp-1");
+  });
+
+  // Passa da etapa 2 para a etapa 1
+  $("#btnRequestHBack2").click(() => {
+    console.log("click botao 1");
+
+    switchStep(".body-requestHelp-3", ".body-requestHelp-2");
+    modalTagsRH.innerHTML = "";
+  });
+
+  $("#btnRequestHBack3").click(() => {
+    console.log("click botao 1");
+
+    switchStep(".body-requestHelp-4", ".body-requestHelp-3");
+  });
+
+  // Se qualquer modal fechar, o display deve ser do body 1
+  $("#eventModalOneToOne").on("hidden.bs.modal", () => {
+    setTimeout(() => {
+      $(".modal-select").removeClass("active");
+      $(".body-oneToOne-1").addClass("active");
+    }, 100);
+  });
+});
---
+++ src/APP.Platform/wwwroot/js/RequestHelpEvent/ValidacaoRequestHelp.js
@@ -0,0 +1,193 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM totalmente carregado e analisado.");
+
+  let hoursStartRH = document.getElementById("hoursStartRH");
+  let minutesStartRH = document.getElementById("minutesStartRH");
+  let isAmStartRH = document.getElementById("ampmStartRH");
+  let inicioHorarioRH = `${
+    hoursStartRH.value < 10 ? "0" + hoursStartRH.value : hoursStartRH.value
+  }:${
+    minutesStartRH.value < 10
+      ? "0" + minutesStartRH.value
+      : minutesStartRH.value
+  }`;
+
+  let hoursEndRH = document.getElementById("hoursEndRH");
+  let minutesEndRH = document.getElementById("minutesEndRH");
+  let isAmEndRH = document.getElementById("ampmEndRH");
+  let fimHorarioRH = `${
+    hoursEndRH.value < 10 ? "0" + hoursEndRH.value : hoursEndRH.value
+  }:${minutesEndRH.value < 10 ? "0" + minutesEndRH.value : minutesEndRH.value}`;
+
+  let dateFormRH = document.getElementById("dateTimeRH");
+  let btnNext3 = document.getElementById("btnRequestHelp3");
+  let errorSpanDateRH = document.getElementById("errorMsgDateRH");
+  let errorSpanTimeRH = document.getElementById("errorMsgTimeRH");
+  let primeiroBotaoRH = document.getElementById("btnRequestHelp1");
+  let tituloMentoriaRH = document.getElementById(
+    "TimeSelectionMentoriaTituloRH"
+  );
+
+  if (
+    !dateFormRH ||
+    !btnNext3 ||
+    !errorSpanDateRH ||
+    !errorSpanTimeRH ||
+    !primeiroBotaoRH ||
+    !tituloMentoriaRH
+  ) {
+    console.error("Elementos essenciais não encontrados na página.");
+    return;
+  }
+  const TWENTY_FOUR_HOURS = 24 * 60 * 60 * 1000;
+  const ONE_MONTH = 30 * 24 * 60 * 60 * 1000;
+  const TWO_HOURS = 2 * 60 * 60 * 1000;
+  const THIRTY_MINUTES = 30 * 60 * 1000;
+
+  function showMessage(message, isError) {
+    if (isError) {
+      errorSpanTimeRH.innerText = message;
+      errorSpanTimeRH.style.display = "block";
+    } else {
+      errorSpanTimeRH.innerText = "";
+      errorSpanTimeRH.style.display = "none";
+    }
+  }
+
+  function validateDateTime(date, firstTime, lastTime) {
+    const currentDate = new Date();
+    const eventDate = new Date(new Date(date).getTime() + TWENTY_FOUR_HOURS);
+    eventDate.setHours(firstTime.getHours());
+    eventDate.setMinutes(firstTime.getMinutes());
+
+    const eventEndDate = new Date(new Date(date).getTime() + TWENTY_FOUR_HOURS);
+    eventEndDate.setHours(lastTime.getHours());
+    eventEndDate.setMinutes(lastTime.getMinutes());
+
+    console.log(eventDate);
+
+    if (date === "") {
+      showMessage("Data inválida.", true);
+      return false;
+    } else if (eventDate.getTime() - currentDate.getTime() > ONE_MONTH) {
+      showMessage(
+        "Um evento não pode ser agendado com mais de um mês de antecedência.",
+        true
+      );
+      return false;
+    } else if (eventDate.getTime() < currentDate.getTime()) {
+      showMessage(
+        "De volta ao futuro? A data do evento não pode ser no passado.",
+        true
+      );
+      return false;
+    } else if (
+      firstTime.getTime() >= lastTime.getTime() ||
+      lastTime.getTime() - firstTime.getTime() > TWO_HOURS ||
+      lastTime.getTime() - firstTime.getTime() < THIRTY_MINUTES
+    ) {
+      showMessage("Os horários são inválidos.", true);
+      return false;
+    } else {
+      showMessage("", false);
+      return true;
+    }
+  }
+
+  const validateDatesRH = () => {
+    let hoursStartInt = parseInt(hoursStartRH.value, 10);
+    console.log(isAmStartRH.value);
+    if (isAmStartRH.value === "pm" && hoursStartInt !== 12) {
+      hoursStartInt += 12;
+    } else if (isAmStartRH.value === "AM" && hoursStartInt === 12) {
+      hoursStartInt = 0; // Ajuste para 12 AM ser 00
+    }
+
+    let minutesInt = parseInt(minutesStartRH.value, 10);
+
+    inicioHorario = `${
+      hoursStartInt < 10 ? "0" + hoursStartInt : hoursStartInt
+    }:${minutesInt < 10 ? "0" + minutesInt : minutesInt}`;
+
+    let hoursEndInt = parseInt(hoursEndRH.value, 10);
+    console.log(isAmEndRH.value);
+    if (isAmEndRH.value === "pm" && hoursEndInt !== 12) {
+      hoursEndInt += 12;
+    } else if (isAmEndRH.value === "AM" && hoursEndInt === 12) {
+      hoursEndInt = 0; // Ajuste para 12 AM ser 00
+    }
+
+    let minutesEndInt = parseInt(minutesEndRH.value, 10);
+
+    fimHorario = `${hoursEndInt < 10 ? "0" + hoursEndInt : hoursEndInt}:${
+      minutesEndInt < 10 ? "0" + minutesEndInt : minutesEndInt
+    }`;
+
+    const startDate = inicioHorarioRH;
+    const endDate = fimHorarioRH;
+    const dateFormValue = dateFormRH.value.trim();
+
+    if (startDate === "" || endDate === "" || dateFormValue === "") {
+      btnNext3.disabled = true;
+      showMessage("Os campos de data e horário não podem estar vazios.", true);
+      return;
+    }
+
+    const startTime = new Date(`1970-01-01T${startDate}`);
+    const endTime = new Date(`1970-01-01T${endDate}`);
+
+    if (!validateDateTime(dateFormValue, startTime, endTime)) {
+      btnNext3.disabled = true;
+      return;
+    }
+
+    btnNext3.disabled = false;
+    errorSpanDateRH.innerText = "";
+    errorSpanDateRH.style.display = "none";
+    errorSpanTimeRH.innerText = "";
+    errorSpanTimeRH.style.display = "none";
+  };
+
+  const validateEventNameRH = () => {
+    return tituloMentoriaRH.value.trim() !== "";
+  };
+
+  const validateEmptyTagsRH = () => {
+    const ulElementForTags = document.getElementById(
+      "select2-TagsSelectedHelp-container"
+    );
+    if (!ulElementForTags) {
+      console.error(
+        "Elemento 'select2-TagsSelectedHelp-container' não encontrado."
+      );
+      return false;
+    }
+    const liElementsForTags = ulElementForTags.querySelectorAll("li");
+    return liElementsForTags.length > 0;
+  };
+
+  const validateFirstModalRH = () => {
+    console.log(validateEmptyTagsRH());
+    console.log(validateEmptyTagsRH());
+    if (validateEventNameRH() && validateEmptyTagsRH()) {
+      primeiroBotaoRH.disabled = false;
+    } else {
+      primeiroBotaoRH.disabled = true;
+    }
+  };
+
+  dateFormRH.addEventListener("change", validateDatesRH);
+  tituloMentoriaRH.addEventListener("input", validateFirstModalRH);
+  $("#TagsSelectedHelp").on("select2:select", validateFirstModalRH);
+  $("#TagsSelectedHelp").on("select2:unselect", validateFirstModalRH);
+
+  hoursStartRH.addEventListener("change", validateDatesRH);
+  minutesStartRH.addEventListener("change", validateDatesRH);
+  isAmStartRH.addEventListener("change", validateDatesRH);
+  hoursEndRH.addEventListener("change", validateDatesRH);
+  minutesEndRH.addEventListener("change", validateDatesRH);
+  isAmEndRH.addEventListener("change", validateDatesRH);
+
+  validateDatesRH();
+  validateFirstModalRH();
+});
