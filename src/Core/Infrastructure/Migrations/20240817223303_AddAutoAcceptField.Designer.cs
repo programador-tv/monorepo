b7b506600e7552281befbe60f94635079abda65a
txsabrina <stx.sabrina@gmail.com>
2024-08-20 21:33:13 -0300

    Adiciona lógica para aceitar interessados automaticamente no Request Help

--- src/Core/Infrastructure/Migrations/20240817223303_AddAutoAcceptField.Designer.cs
+++ src/Core/Infrastructure/Migrations/20240817223303_AddAutoAcceptField.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240817223303_AddAutoAcceptField")]
+    partial class AddAutoAcceptField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
