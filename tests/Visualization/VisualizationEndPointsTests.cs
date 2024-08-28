6cc72c00e21af9d1d5b4648b9d7d44965447c53e
Ruan <75811206+R-uan@users.noreply.github.com>
2024-08-28 06:32:58 -0300

    test coverage

--- tests/Visualization/VisualizationEndPointsTests.cs
+++ tests/Visualization/VisualizationEndPointsTests.cs
@@ -2,6 +2,7 @@ using System;
 using Application.Logic;
 using Domain.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 using Presentation.EndPoints;
 
@@ -17,7 +18,7 @@ namespace tests
         }
 
         [Fact]
-        public async Task GetLiveVisualizationEndpoint_ShouldCallLogic()
+        public async Task GetLiveVisualizationEndpoint_ShouldReturnOkStatus()
         {
             var visualizations = new List<Visualization>
             {
@@ -38,6 +39,28 @@ namespace tests
                 logic => logic.GetLiveVisualizations(It.IsAny<List<Guid>>()),
                 Times.Once()
             );
+
+            Assert.IsType<Ok<List<Visualization>>>(result);
+        }
+
+        [Fact]
+        public async Task GetLiveVisualizationEndpoint_ShouldReturnBadRequestStatus()
+        {
+            mockLogic
+                .Setup(logic => logic.GetLiveVisualizations(It.IsAny<List<Guid>>()))
+                .ThrowsAsync(new Exception("Test error"));
+
+            var result = await VisualizationEndPoints.GetLiveVisualization(
+                mockLogic.Object,
+                [Guid.NewGuid(), Guid.NewGuid()]
+            );
+
+            mockLogic.Verify(
+                logic => logic.GetLiveVisualizations(It.IsAny<List<Guid>>()),
+                Times.Once()
+            );
+
+            Assert.IsType<BadRequest<string>>(result);
         }
     }
 }
