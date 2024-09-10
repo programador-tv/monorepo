dbad6c1ea48ac859db126f61a7072fe7c67798b0
Matheus Caldana <160268301+MatheusCFBT@users.noreply.github.com>
2024-09-10 14:28:33 +0000

    Add EndPoint for Visualization

---
+++ src/Core/Presentation/EndPoints/VisualizationEndPoints.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Presentation.EndPoints;
+
+public static class VisualizationEndPoints
+{
+    public static void AddVisualizationEndPoints(this IEndpointRouteBuilder app)
+    {
+        
+    }
+}
